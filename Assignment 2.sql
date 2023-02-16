94e7dc75c0f92154bcd0911096dad704429c5f9e
nguyenthinhuquynh08 <124903650+nguyenthinhuquynh08@users.noreply.github.com>
2023-02-16 10:26:23 +0700

    Assignment 2
    Assignment 2 _ Quỳnh

--- Assignment 2.sql
+++ Assignment 2.sql
@@ -1,93 +1,112 @@
-/*database testing system*/
+-- Tạo database testing_system
 DROP DATABASE IF EXISTS testing_system;
 
 CREATE DATABASE IF NOT EXISTS testing_system;
 
 USE testing_system;
--- create table
-DROP TABLE IF EXISTS department;
--- Table 1
+
+-- TABLE 1:  department
+
 CREATE TABLE department (
 DepartmentID TINYINT PRIMARY KEY AUTO_INCREMENT,
-DepartmentName VARCHAR(50) NOT NULL UNIQUE
-);
--- Table 2
-CREATE TABLE position (
-PositionID TINYINT PRIMARY KEY AUTO_INCREMENT,
-PositiontName ENUM('Dev', 'Test', 'SCrum Master', 'PM')
-);
--- Table 3
-CREATE TABLE `account` (
-AccountID INT PRIMARY KEY AUTO_INCREMENT,
-Email VARCHAR(50) NOT NULL UNIQUE,
-Usename VARCHAR(50) NOT NULL UNIQUE,
-Fullname VARCHAR(100),
-DepartmentID TINYINT,
-PositionID TINYINT,
-CreateDate date,
-CONSTRAINT account_department_fk FOREIGN KEY (DepartmentID) REFERENCES department(DepartmentID),
-CONSTRAINT account_position_fk FOREIGN KEY (PositionID) REFERENCES `position`(PositionID)
-);
--- Table 4
-CREATE TABLE `group` (
-GroupID INT PRIMARY KEY AUTO_INCREMENT,
-GroupName VARCHAR(100) NOT NULL UNIQUE,
-CreatorID INT,
-CreateDate DATE
-);
--- Table 5
-CREATE TABLE groupaccount(
-GroupID INT,
-AccountID INT,
-JoinDate date,
-CONSTRAINT groupaccount_group_fk FOREIGN KEY (GroupID) REFERENCES `group`(GroupID),
-CONSTRAINT groupaccount_account_fk FOREIGN KEY (AccountID) REFERENCES `account`(AccountID)
-);
--- Table 6
-CREATE TABLE typequestion(
-TypeID INT PRIMARY KEY AUTO_INCREMENT,
-Typename ENUM ('Essay', 'Multiple-Choice')
-);
--- Table 7
-CREATE TABLE categoryquestion(
-CategoryID INT AUTO_INCREMENT PRIMARY KEY,
-CategoryName VARCHAR(20) NOT NULL
-);
--- Table 8
-CREATE TABLE question(
-QuestionID INT PRIMARY KEY AUTO_INCREMENT,
-Content  VARCHAR(500) NOT NULL,
-CategoryID INT NOT NULL,
-TypeID INT NOT NULL,
-CreatorID INT NOT NULL,
-CreateDate DATE,
-CONSTRAINT question_typequestion_fk FOREIGN KEY (TypeID) REFERENCES typequestion(TypeID),
-CONSTRAINT question_categoryquestion_fk FOREIGN KEY (CategoryID) REFERENCES categoryquestion(CategoryID)
-);
--- Table 9
-CREATE TABLE answer(
-AnswerID INT PRIMARY KEY AUTO_INCREMENT,
-Content VARCHAR(300) NOT NULL,
-QuestionID INT NOT NULL,
-isCorrect BIT,
-CONSTRAINT answer_question_fk FOREIGN KEY (QuestionID) REFERENCES question(QuestionID)
-);
--- Table 10
-CREATE TABLE exam(
-ExamID INT PRIMARY KEY AUTO_INCREMENT,
-`Code` VARCHAR(10) NOT NULL,
-Title VARCHAR(100) NOT NULL,
-CategoryID INT NOT NULL,
-Duration TINYINT NOT NULL,
-CreatorID INT NOT NULL,
-CreateDate DATE NOT NULL,
-CONSTRAINT exam_categoryquestion_fk FOREIGN KEY (CategoryID) REFERENCES categoryquestion(CategoryID)
-);
--- Table 11
-CREATE TABLE examquestion(
-ExamID INT NOT NULL,
-QuestionID INT NOT NULL,
-CONSTRAINT examquestion_exam_fk FOREIGN KEY (ExamID) REFERENCES exam(ExamID),
-CONSTRAINT examquestion_question_ fk FOREIGN KEY (QuestionID) REFERENCES question(QuestionID)
+DepartmentName VARCHAR(200) NOT NULL UNIQUE
 );
 
+-- TABLE 2: Position
+
+CREATE TABLE position (
+PositionID TINYINT PRIMARY KEY AUTO_INCREMENT,
+PositionName ENUM ('Dev', 'Test', 'Scrum Master', 'PM')
+);
+
+-- TABLE 3:  Account
+
+CREATE TABLE `account` (
+AccountID TINYINT PRIMARY KEY AUTO_INCREMENT,
+Email VARCHAR(200),
+Username VARCHAR(50),
+FullName VARCHAR(200),
+DepartmentID TINYINT DEFAULT NULL,
+PositionID TINYINT DEFAULT NULL,
+CreateDate DATE,
+CONSTRAINT account_department_fk FOREIGN KEY (DepartmentID) REFERENCES department(DepartmentID) ON DELETE SET NULL ON UPDATE CASCADE,
+CONSTRAINT account_position_fk FOREIGN KEY (PositionID) REFERENCES `position`(PositionID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- TABLE 4:  Group
+
+CREATE TABLE `group` (
+GroupID TINYINT PRIMARY KEY AUTO_INCREMENT,
+GroupName VARCHAR(100),
+CreatorID SMALLINT,
+CreateDate DATE
+);
+
+-- TABLE 5: GroupAccount
+
+CREATE TABLE groupaccount (
+GroupID TINYINT DEFAULT NULL,
+AccountID TINYINT DEFAULT NULL,
+JoinDate DATETIME DEFAULT NOW(),
+CONSTRAINT groupaccount_group_fk FOREIGN KEY (GroupID) REFERENCES `group`(GroupID) ON DELETE SET NULL ON UPDATE CASCADE,
+CONSTRAINT groupaccount_account_fk FOREIGN KEY (AccountID) REFERENCES `account`(AccountID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- TABLE 6: TYPEQUESTION
+
+CREATE TABLE typequestion (
+TypeID TINYINT PRIMARY KEY AUTO_INCREMENT,
+TypeName ENUM ('Essay', 'Multiple-Choice')
+);
+
+-- Table 7: CategoryQuestion
+
+CREATE TABLE categoryquestion (
+CategoryID TINYINT PRIMARY KEY AUTO_INCREMENT,
+CategoryName ENUM ('Java', 'SQL', '.NET', 'Ruby', 'Python', 'NodeJS', 'HTML', 'CSS', 'JavaScript')
+);
+
+-- Table 8: Question
+
+CREATE TABLE question (
+QuestionID TINYINT PRIMARY KEY AUTO_INCREMENT,
+Content VARCHAR(200),
+CategoryID TINYINT DEFAULT NULL,
+TypeID TINYINT DEFAULT NULL,
+CreatorID SMALLINT,
+CreateDate DATETIME DEFAULT NOW(),
+CONSTRAINT question_categoryquestion_fk FOREIGN KEY (CategoryID) REFERENCES categoryquestion(CategoryID) ON DELETE SET NULL ON UPDATE CASCADE,
+CONSTRAINT question_typequestion_fk FOREIGN KEY (TypeID) REFERENCES typequestion(TypeID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Table 9: Answer
+
+CREATE TABLE answer (
+AnswerID TINYINT PRIMARY KEY AUTO_INCREMENT,
+Content VARCHAR(200),
+QuestionID TINYINT DEFAULT NULL,
+isCorrect BIT,
+CONSTRAINT answer_question_fk FOREIGN KEY (QuestionID) REFERENCES question(QuestionID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Table 10: Exam
+
+CREATE TABLE exam (
+ExamID TINYINT PRIMARY KEY AUTO_INCREMENT,
+`Code` VARCHAR(10),
+Title VARCHAR(100),
+CategoryID TINYINT DEFAULT NULL,
+Duration TINYINT,
+CreatorID SMALLINT DEFAULT NULL,
+CreateDate DATETIME DEFAULT NOW(),
+CONSTRAINT exam_categoryquestion_fk FOREIGN KEY (CategoryID) REFERENCES categoryquestion(CategoryID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- Table 11: ExamQuestion
+
+CREATE TABLE examquestion (
+ExamID TINYINT,
+QuestionID TINYINT,
+CONSTRAINT examquestion_exam_fk FOREIGN KEY (ExamID) REFERENCES exam(ExamID) ON DELETE SET NULL ON UPDATE CASCADE,
+CONSTRAINT examquestion_question_fk FOREIGN KEY (QuestionID) REFERENCES question(QuestionID) ON DELETE SET NULL ON UPDATE CASCADE
+);
